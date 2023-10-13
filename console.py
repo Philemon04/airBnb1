8a1dacf18e95024ae08e387da1d9651f990aa12e
Philemon04 <philmuthomi04@gmail.com>
2023-10-13 17:02:05 +0300

    commit

--- console.py
+++ console.py
@@ -2,10 +2,13 @@
 
 import cmd
 
+from models import storage
 from models.base_model import BaseModel
 from models.engine import file_storage
 
 classes = {"BaseModel": BaseModel}
+
+
 class HBNBCommand(cmd.Cmd):
     prompt = '(hnhb)'
 
@@ -23,21 +26,39 @@ class HBNBCommand(cmd.Cmd):
         @rtype: object
         """
         return True
+
     def do_create(self, args):
 
         arg = args.split()
 
-        if arg<=0:
+        if arg <= 0:
             print('** class name missing **')
 
         if arg in classes:
+
             new_dict = arg[1:]
             instance = classes[args[0]](**new_dict)
         else:
             print("** class doesn't exist **")
-        instance.save()
 
+    def do_show(self, args):
+        arg = args.split()
+        all_objects = storage.all()
+        instance = args[0] + '.' + args[1]
+        if len(arg) <= 0:
+            print('** class name missing **')
 
+        elif arg[0] not in classes:
+            print("** class doesn't exist **")
+
+        elif len(arg) > 2:
+            print('** instance id missing **')
+
+        elif instance in all_objects:
+            print(all_objects[instance])
+
+        else:
+            print("** no instance found **")
 
 
 if __name__ == '__main__':
--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,24 @@
+#!/usr/bin/python3
 from _datetime import datetime
 import uuid
 
+from models import storage
+
 
 class BaseModel:
-    id = uuid
-    created_at = datetime.today()
-    updated_at = datetime.today()
+    def __init__(self, *args, **kwargs):
+        self.id = uuid
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    if key != '__class__':
+                        self.__dict__[key] = value
+        else:
+            storage.new(self)
 
     def __str__(self):
         classname = self.__class__.__name__
@@ -13,7 +26,11 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         new_dict = self.__dict__.copy()
+        new_dict['created_at'] = datetime.strftime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+        new_dict['updated_at'] = datetime.strftime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+        new_dict['__class__'] = self.__class__.__name__
         return new_dict
